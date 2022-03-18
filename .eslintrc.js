169cc6362bbaa13745a07fc0ba82949654068868
Andres Galvan <andresgalfajar@gmail.com>
2022-03-17 22:55:17 -0500

    fix: add jest environment to the linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,18 @@ module.exports = {
   env: {
     es2021: true,
     browser: true,
+    jest: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['jest.config*', 'webpack*', '.eslint*'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'simple-import-sort', 'html', 'import'],
+  plugins: ['simple-import-sort', 'html', 'import'],
   rules: {
-    'max-len': [2, { code: 95, ignoreUrls: true }],
+    'max-len': [2, { code: 80, ignoreUrls: true }],
     indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
